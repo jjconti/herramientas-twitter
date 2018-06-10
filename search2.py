b883bd60eabf3a710d96bef838f2347de4549d0b
Juanjo Conti <jjconti@gmail.com>
2018-06-10 18:50:21 -0300

    Fix search2

--- search2.py
+++ search2.py
@@ -1,20 +1,15 @@
 from TwitterSearch import *
-from data import api
+from login import *
 
 try:
     tso = TwitterSearchOrder()
     tso.set_keywords(['phyton'])
-    #tso.set_language('es')
     tso.set_include_entities(False)
-
-
-
     ts = TwitterSearch(
-        consumer_key = 'A7rnVb7Ad4YdLraaQWMRnKTeU',
-        consumer_secret = 'fvnDJCeAPPO6CEG2RAXNRoNiN2qO4FqQxrJLNnb3cpAH1oi2EL',
-        access_token = '1002626003846475776-L1vFAajHz4rTqpVhRroDBCDOCqIobj',
-        access_token_secret = 'Dqmm7bjrsjgFEvU6EtyRpjUGDnjyF3uFwfAkWcHr2aQDE'
-
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token=access_key,
+        access_token_secret=access_secret
     )
 
     cuenta = 0
@@ -23,17 +18,10 @@ try:
         usuario = tweet["user"]["screen_name"]
         status_id = tweet["id"]
         if "phyton" in tweet["text"]:
-            #import ipdb;ipdb.set_trace()
-            api.PostUpdate("@{} It's Python!".format(usuario), in_reply_to_status_id=status_id, auto_populate_reply_metadata=True)
+            api.PostUpdate("@{} It's Python!".format(usuario), in_reply_to_status_id=status_id,
+                           auto_populate_reply_metadata=True)
             cuenta += 1
-            print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
-
-
     print(cuenta)
-    
-            
-
-
 
 except TwitterSearchException as e:
     print(e)
